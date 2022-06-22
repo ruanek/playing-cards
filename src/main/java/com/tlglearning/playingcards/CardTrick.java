f54eb72db5449171979ed3ca4e1f68a443662259
Kyle Ruane <ruanek@amazon.com>
2022-06-22 09:11:24 -0700

    Implemented sorting function in tally.

--- src/main/java/com/tlglearning/playingcards/CardTrick.java
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -3,10 +3,9 @@ package com.tlglearning.playingcards;
 import com.tlglearning.playingcards.model.Card;
 import com.tlglearning.playingcards.model.Deck;
 import com.tlglearning.playingcards.model.Suit;
+import com.tlglearning.playingcards.util.DisplayComparator;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Random;
+import java.util.*;
 
 public class CardTrick {
 
@@ -28,8 +27,6 @@ public class CardTrick {
         // TODO (Optional: Shuffle the red deque and black deque)
 
         trick.swapCards();
-
-        // TODO Sort each deque by color, suit, and rank,
 
         trick.tally();
 
@@ -69,6 +66,9 @@ public class CardTrick {
                 redCount++;
             }
         }
+        Comparator<Card> comparator = new DisplayComparator();
+        Collections.sort((LinkedList<Card>)blackPile, comparator);
+        Collections.sort((LinkedList<Card>)redPile, comparator);
         System.out.printf("Black: count=%d, cards=%s%n", blackCount, blackPile);
         System.out.printf("Red: count=%d, cards=%s%n", redCount, redPile);
     }
